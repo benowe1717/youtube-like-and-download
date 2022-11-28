539234a0b5346a5e71b6d7e78ab1f589ccff03bb
Benjamin Owen <accounts@projecttiy.com>
2022-11-28 18:41:56 -0500

    completely reworked the youtubeOauth class to conform closer with oop methodologies and to separate the "liking" of a video from the "authentication". those should not be in the same class

--- youtubeOauth.py
+++ youtubeOauth.py
@@ -1,133 +1,147 @@
 #!/usr/bin/env python3
-import requests, json, time
+import requests, json
 from urllib.parse import urlencode
-from youtubeDL import youtubeDL
 
-class youtubeOauth(youtubeDL):
+class youtubeOauth():
     'This class serves to support the youtubeDL class to get Oauth tokens for the YouTube API'
-    RATINGS = ["like", "dislike", "none"]
-    SCOPE = "https://www.googleapis.com/auth/youtube"
+
+    #########################
+    ### PRIVATE CONSTANTS ###
+    #########################
+    _SECRETS_FILE = "client_secrets.json"
+    _REFRESH_TOKEN_FILE = "refresh_token.txt"
+
+    ########################
+    ### PUBLIC CONSTANTS ###
+    ########################
+    NEW_AUTH = False
     SCHEME = "https://"
     BASE_URL = "oauth2.googleapis.com"
-    HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
-    SECRETS = "client_secrets.json"
-    REFRESHER = "refresh_token.txt"
+    SCOPE = "https://www.googleapis.com/auth/youtube"
+    GRANT_TYPE = "urn:ietf:params:oauth:grant-type:device_code" # https://developers.google.com/youtube/v3/guides/auth/devices#step-4:-poll-googles-authorization-server
 
-    video_ids = []
-    rating = ""
-    client_id = ""
-    client_secret = ""
-    access_token = ""
+    #######################
+    ### PRIVATE OBJECTS ###
+    #######################
+    _client_id = ""
+    _client_secret = ""
+    _access_token = ""
+    _refresh_token = ""
 
-    def __init__(self, rating):
-        # Pull in the parent's methods and objects
-        # so that we can access them as self.
-        super().__init__()
+    ######################
+    ### PUBLIC OBJECTS ###
+    ######################
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    # device_codes will always be in device_code, user_code, verification_url, interval order
+    device_codes = [] # https://developers.google.com/youtube/v3/guides/auth/devices#step-2:-handle-the-authorization-server-response
 
+
+    # NOTE that all print statements will become
+    # actual log messages once i've rewritten the log method to 
+    # be an actual class
+
+    def __init__(self):
         try:
-            with open(self.SECRETS, "r") as f:
-                json_data = json.loads(f.readlines()[0])
-                self.client_id = json_data["installed"]["client_id"]
-                self.client_secret = json_data["installed"]["client_secret"]
-
-            if rating in self.RATINGS:
-                self.rating = rating
-            else:
-                self.logMsg("ERROR: Invalid rating!")
-                exit(1)
+            with open(self._SECRETS_FILE, "r") as file:
+                json_data = json.loads(file.readlines()[0])
+                self._client_id = json_data["installed"]["client_id"]
+                self._client_secret = json_data["installed"]["client_secret"]
         except FileNotFoundError:
-            self.logMsg("ERROR: Unable to locate client_secrets.json file! Cannot continue!")
-            self.logMsg("You either need to download the client_secrets.json file from the Google Cloud console or move that file into the same directory as this script!", 1)
+            print("ERROR: Unable to locate client_secrets.json file!")
+            print("You either need to download the client_secrets.json file from the Google Cloud console or update the _SECRETS_FILE constant with the appropriate path...")
             exit(1)
 
-    def updateRefreshToken(self, refresh_token):
-        # https://developers.google.com/youtube/v3/guides/auth/devices#offline
-        with open(self.REFRESHER, "w") as f:
-                f.write(refresh_token)
+        try:
+            with open(self._REFRESH_TOKEN_FILE, "r") as file:
+                self._refresh_token = file.readlines()[0]
+        except FileNotFoundError:
+            print("ERROR: Unable to locate the refresh_token.txt file!")
+            print("This could be due to this being the first time we are being authorized OR you need to update the _REFRESH_TOKEN_FILE constant with the appropriate path...")
+            self.NEW_AUTH = True
+            # no exit here as we can just reauth based on what happens
+
+    def _saveRefreshToken(self):
+        'This method is used by the pollAuthServer method to save a local copy of the refresh_token so that it can be used to get a new access_token on subsequent runs'
+        with open(self._REFRESH_TOKEN_FILE, "w") as file:
+            file.writelines(self._refresh_token)
 
     def requestDeviceAndUserCodes(self):
+        'This method sends an HTTP POST request to the authorization server to request Device and User codes for OAuth authentication'
         # https://developers.google.com/youtube/v3/guides/auth/devices#step-1:-request-device-and-user-codes
         endpoint = "/device/code"
         url = self.SCHEME + self.BASE_URL + endpoint
-        data = {"client_id": self.client_id, "scope": self.SCOPE}
-        r = requests.post(url=url, headers=self.HEADERS, data=urlencode(data))
+        data = {"client_id": self._client_id, "scope": self.SCOPE}
+        print(f"DEBUG: Calling YouTube API via URL: {url}...")
+        r = requests.post(url=url, headers=self.headers, data=urlencode(data))
+        json_data = json.loads(r.text)
         if r.status_code == 200:
-            self.logMsg("Successfully retrieved device and user codes!")
-            return json.loads(r.text)
+            print("Successfully retrieved device and user codes!")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text}")
+            # Apparently the .append() method only accepts one argument, and I wanted to
+            # try to keep this to one line, so I found this article:
+            # https://bobbyhadz.com/blog/python-append-multiple-values-to-list-in-one-line
+            self.device_codes.extend([json_data["device_code"], json_data["user_code"], json_data["verification_url"], json_data["interval"]])
+            return True
+        elif r.status_code == 403:
+            if json_data["error_code"] == "rate_limit_exceeded":
+                print("ERROR: API Quota has been exceeded for this account!")
+                print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text}")
         else:
-            self.logMsg("ERROR: Unable to retrieve device and user codes!")
-            self.logMsg("DEBUG: Client ID: %s :: HTTP Response: %d :: Error Text: %s" % (self.client_id, r.status_code, r.text), 1)
+            print("ERROR: Unable to request Device and User Codes!")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text}")
+            return False
 
-    def displayUserCode(self, response):
+    def displayUserCode(self):
+        'This method displays the Veritifcation URL and the User Code obtained from the requestDeviceAndUserCodes() method'
         # https://developers.google.com/youtube/v3/guides/auth/devices#displayingthecode
-        print("Please navigate to this URL: %s and input the following code: %s" % (response["verification_url"], response["user_code"]))
+        print(f"Please navigate to the following URL: {self.device_codes[2]} and enter the following code: {self.device_codes[1]}")
 
-    def pollAuthServer(self, response):
+    def pollAuthServer(self):
+        'This method polls the authorization server at the specified interval to determine if the user has input the correct user_code and allowed our app to authenticate on their behalf'
         # https://developers.google.com/youtube/v3/guides/auth/devices#step-4:-poll-googles-authorization-server
-        self.logMsg("Waiting for user to authorize this script...")
-        time.sleep(response["interval"])
-
         endpoint = "/token"
         url = self.SCHEME + self.BASE_URL + endpoint
-        data = {"client_id": self.client_id, "client_secret": self.client_secret, "device_code": response["device_code"], "grant_type": "urn:ietf:params:oauth:grant-type:device_code"}
-        r = requests.post(url=url, headers=self.HEADERS, data=urlencode(data))
-        result = self.handlePollingServerResponse(r)
-        while result is False:
-            self.logMsg("Still waiting for user to authorize this script...")
-            time.sleep(response["interval"])
-            result = self.handlePollingServerResponse(r)
-        self.logMsg("User successfully authorized the script!")
-        self.access_token = result["access_token"]
-
-    
-    def handlePollingServerResponse(self, response):
-        # https://developers.google.com/youtube/v3/guides/auth/devices#step-6:-handle-responses-to-polling-requests
-        if response.status_code == 200:
-            json_data = json.loads(response.text)
-            self.updateRefreshToken(json_data["refresh_token"])
-            return json_data
+        data = {"client_id": self._client_id, "client_secret": self._client_secret, "device_code": self.device_codes[0], "grant_type": self.GRANT_TYPE}
+        print(f"DEBUG: Calling YouTube API via URL: {url}...")
+        r = requests.post(url=url, headers=self.headers, data=urlencode(data))
+        json_data = json.loads(r.text)
+        if r.status_code == 200:
+            print("User has successfully authorized our application!")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text}")
+            self._access_token = json_data["access_token"]
+            self._refresh_token = json_data["refresh_token"]
+            self._saveRefreshToken()
+            return 200 # This signals the end of use for this method
+        elif r.status_code == 428:
+            print(f"User has not completed the authorization flow! Will check again in {self.device_codes[3]} seconds...")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Error: {json_data['error']} :: Error Description: {json_data['error_description']}")
+            return 428 # This signals that the method should be used again once the specified interval has passed
+        elif r.status_code == 403:
+            print(f"ERROR: {json_data['error']} has occurred! Description: {json_data['error_description']}!")
+            if json_data["error"] == "slow_down":
+                return 425 # This signals something has gone wrong with the speed of the requests, try tripling the wait time
+            else:
+                return 403 # This signals something has gone wrong with the method and probably isn't recoverable
         else:
-            return False
+            print(f"ERROR: Unable to contact YouTube API!")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text}")
+            return 1 # This signals something has gone wrong with the method and is not recoverable
 
     def refreshAccessToken(self):
+        'This method is used to refresh the current Access Token as these tokens periodically expire and become invalid after expiration'
         # https://developers.google.com/youtube/v3/guides/auth/devices#offline
-        try:
-            with open(self.REFRESHER, "r") as f:
-                refresh_token = f.readlines()[0]
-            
-            endpoint = "/token"
-            url = self.SCHEME + self.BASE_URL + endpoint
-            data = {"client_id": self.client_id, "client_secret": self.client_secret, "refresh_token": refresh_token, "grant_type": "refresh_token"}
-            r = requests.post(url=url, headers=self.HEADERS, data=data)
-            if r.status_code == 200:
-                # With the refresh_token saved and stored, it should be reuseable and this should be the 
-                # most used part of this method
-                self.logMsg("Access token successfully refreshed!")
-                json_data = json.loads(r.text)
-                self.access_token = json_data["access_token"]
-            else:
-                # If the refresh_token file has been corrupted or not saved properly or if the
-                # refresh_token is simply incorrect or typod, then we need to get a brand new
-                # access token and refresh token
-                self.logMsg("ERROR: Unable to refresh the access_token with the refresh_token we have on file!")
-                self.logMsg("DEBUG: Refresh Token: %s :: HTTP Response: %d :: Error Details: %s" % (refresh_token, r.status_code, r.text), 1)
-                return False
-        except FileNotFoundError:
-            # If the refresh_token file isn't even on the system, then this is probably the first time
-            # this script has been ran on the system, or by the user, so we need to get new codes
-            self.logMsg("ERROR: Unable to refresh the access_token as there is no refresh_token on file!")
-            self.logMsg("This must be a new authorization attempt...")
+        endpoint = "/token"
+        url = self.SCHEME + self.BASE_URL + endpoint
+        data = {"client_id": self._client_id, "client_secret": self._client_secret, "grant_type": "refresh_token", "refresh_token": self._refresh_token}
+        print(f"DEBUG: Calling YouTube API via URL: {url}...")
+        r = requests.post(url=url, headers=self.headers, data=urlencode(data))
+        json_data = json.loads(r.text)
+        if r.status_code == 200:
+            print("Successfully refreshed our Access Token!")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text}")
+            self._access_token = json_data["access_token"]
+            return True
+        else:
+            print("ERROR: Unable to refresh our Access Token or unable to contact the YouTube API!")
+            print(f"DEBUG: HTTP Response Code: {r.status_code} :: Response Text: {r.text} :: Refresh Token: {self._refresh_token}")
             return False
-
-    def rateVideos(self):
-        # https://developers.google.com/youtube/v3/docs/videos/rate
-        for i in self.to_download:
-            url = "https://youtube.googleapis.com/youtube/v3/videos/rate?id=%s&rating=%s&key=%s" % (i, self.rating, self._apikey)
-            headers = {"Accept": "application/json", "Authorization": ""}
-            headers["Authorization"] = "Bearer %s" % self.access_token
-            r = requests.post(url=url, headers=headers)
-            if r.status_code == 204:
-                self.logMsg("Successfully liked video: %s!" % i)
-            else:
-                self.logMsg("ERROR: Unable to leave rating on video!")
-                self.logMsg("DEBUG: Access Token: %s :: HTTP Response: %d :: Error Text: %s :: Video ID: %s" % (self.access_token, r.status_code, r.text, i), lvl=1)
